c2b379670e82a45977c6fc15db8386f05840df40
Maria Kononchuk <mariyka.kononchuk@gmail.com>
2021-07-12 19:14:55 +0300

    добавила стили в таймер

--- js/timer/timer.js
+++ js/timer/timer.js
@@ -1,30 +1,3 @@
-
-//переводит мс в формат день:час:мин:сек
-function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-    // Remaining days
-    const days = pad(Math.floor(ms / day));
-    // Remaining hours
-    const hours = pad(Math.floor((ms % day) / hour));
-    // Remaining minutes
-    const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-    // Remaining seconds
-    const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
-
-    return { days, hours, minutes, seconds };
-}
-
-//добавляет нолик в формат даты 00:00:00:00 вместо 0:0:0:0
-function pad(value) {
-    return String(value).padStart(2, '0');
-};
-
-
 const refs = {
     startBtn: document.querySelector('button[data-start]'),
     inputData: document.getElementById("date-selector"),
@@ -34,7 +7,8 @@ const refs = {
     timerSeconds: document.querySelector('span[data-seconds]'),
 }
 
-refs.inputData.addEventListener("change", onDataInput);
+refs.inputData.addEventListener("input", onDataInput);
+
 refs.startBtn.addEventListener('click', () => {
     if (finishTime <= Date.now()) {
         Swal.fire({
@@ -48,15 +22,9 @@ refs.startBtn.addEventListener('click', () => {
 
 let finishTime = 0;
 function onDataInput() {
-    const input = this.value;
-    const dateEntered = new Date(input);
+    const dateEntered = new Date(this.value);
     finishTime = dateEntered.getTime();
-    // console.log(dateEntered.getTime());
-    // console.log(Date.now());
-    // console.log(input); //e.g. 2015-11-13
-    // console.log(finishTime); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
-    // console.log(convertMs(finishTime));
-    // console.log(convertMs(Date.now()));
+    refs.inputData.setAttribute("readonly", 1)
 };
 
 class Timer {
@@ -70,22 +38,15 @@ class Timer {
         if (this.isActive) { //проверяет если таймер уже активен, то не дает его запустить снова
             return;
         }
-
         refs.startBtn.disabled = true; //если таймер запускается -кнопка запуска становится неактивной
         this.intervalId = setInterval(() => {  //intervalId необходим чтоб остановить таймер(обратиться к нему)
             const currentTime = Date.now();
-            //console.log(currentTime);
-            // console.log(finishTime);
             const deltaTime = finishTime - currentTime - 10800000;
-            //console.log(deltaTime);
-            // const { days, hours, minutes, seconds } = convertMs(deltaTime);
-            const time = convertMs(deltaTime); //конвертирует мс в правильный формат даты
-            //console.log(time);
-            if (deltaTime === null) {
+            const time = this.convertMs(deltaTime); //конвертирует мс в правильный формат даты
+            if (deltaTime == null) {
                 stop()
             };
             this.onTick(time); //передает структурирован параметры даты 
-
         }, 1000);
     }
 
@@ -94,10 +55,35 @@ class Timer {
         this.isActive = false;
         startBtn.disabled = false;
     }
+
+    //переводит мс в формат день:час:мин:сек
+    convertMs(ms) {
+        // Number of milliseconds per unit of time
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+
+        // Remaining days
+        const days = this.pad(Math.floor(ms / day));
+        // Remaining hours
+        const hours = this.pad(Math.floor((ms % day) / hour));
+        // Remaining minutes
+        const minutes = this.pad(Math.floor(((ms % day) % hour) / minute));
+        // Remaining seconds
+        const seconds = this.pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+        return { days, hours, minutes, seconds };
+    }
+
+    //добавляет нолик в формат даты 00:00:00:00 вместо 0:0:0:0
+    pad(value) {
+        return String(value).padStart(2, '0');
+    };
 }
 
 const timer = new Timer({
-    onTick: updateClockFace
+    onTick: updateClockFace //передаем как свойство таймера
 });
 
 //добавляет время таймера в интерфейс (HTML)
